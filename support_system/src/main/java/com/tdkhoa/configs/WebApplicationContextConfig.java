27036f40732045e5f9cc2fa15ffa63a72aaa2c47
Trần Đăng Khoa <tdkhoa1101@gmail.com>
2023-07-10 21:53:08 +0700

    first commit

---
+++ support_system/src/main/java/com/tdkhoa/configs/WebApplicationContextConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tdkhoa.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author Khoa Tran
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.tdkhoa.controllers")
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+}
---
+++ support_system/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : Jul 10, 2023, 9:33:03 PM
+    Author     : Khoa Tran
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Homepage</title>
+    </head>
+    <body>
+        <h1>Hello World! Welcome to website</h1>
+    </body>
+</html>
+
+
+
